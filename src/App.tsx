9b73f123ee4f0b8d72f4d5045f0b233d076257dc
Kuldeep singh sekhon <mca.kuldeep.sekhon@gmail.com>
2025-01-06 22:17:37 +0530

    saved to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import TodoInput from './components/TodoInput'
 import TodoList from './components/Todolist'
@@ -6,19 +6,38 @@ import TodoList from './components/Todolist'
 function App() {
   const [todolist, setTodolist]=useState([])
   const [todo,setTodo]=useState('');
+  useEffect(()=>{
+    // if(!localStorage){
+    //   return true;
+    // }
+    // if(!localStorage.getItem('todolist')){
+    //   return true;
+    // }
+    const data=localStorage.getItem('todolist');
+    if(data){
+      setTodolist(JSON.parse(data))
+    }
+  },[]);
+  function saveToLocalStorage(todolist:string[]){
+    localStorage.setItem('todolist',JSON.stringify(todolist))
+  }
+
   function handleAddTodo(newTodo:string){
     setTodolist([...todolist,newTodo]);
+    saveToLocalStorage([...todolist,newTodo]);
     }
     function deleteTodo(delindex:number){
       const newtodos=todolist.filter((todo:string,index:number)=>{
         return index!==delindex
+       
       })
       setTodolist(newtodos)
+      saveToLocalStorage(newtodos);
     }
     function editTodo(todo:string,index:number){
       const todoToedit=todolist[index];
       setTodo(todoToedit);
-      deleteTodo(index)
+      deleteTodo(index);
       console.log(todo,index)
     }
   return (
